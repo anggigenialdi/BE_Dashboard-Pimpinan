b9812feaa30fb8ff8988a4fffdae9188e28ede4e
Anggi Genialdi <anggigenialdi@gmail.com>
2022-03-11 14:59:39 +0700

    function boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,4 +19,12 @@ class AppServiceProvider extends ServiceProvider
     }
 
 
+    public function boot()
+    {
+        if (env('APP_ENV') === 'production') {
+            $this->app['request']->server->set('HTTPS','on'); // this line
+    
+            URL::forceSchema('https');
+        }
+    }
 }
