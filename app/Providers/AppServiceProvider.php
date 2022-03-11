903217592c76ef312c3265fd4f164a597d3afec0
Anggi Genialdi <anggigenialdi@gmail.com>
2022-03-11 15:09:15 +0700

    data update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,13 +18,10 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-
     public function boot()
     {
-        if (env('APP_ENV') === 'production') {
-            $this->app['request']->server->set('HTTPS','on'); // this line
-    
-            URL::forceSchema('https');
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
         }
     }
 }
