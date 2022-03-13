cb83410e7665b93819333978c82b1d80d3a9ae83
Anggi Genialdi <anggigenialdi@gmail.com>
2022-03-13 19:55:55 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -97,7 +97,7 @@ $app->routeMiddleware([
 $app->register(\SwaggerLume\ServiceProvider::class);
 
 
-// $app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
