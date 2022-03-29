7106009db17ef6b58e54d100546e98e6b38d87e4
Anggi Genialdi <anggigenialdi@gmail.com>
2022-03-29 13:20:18 +0700

    data

--- app/Http/Controllers/MasterDataCctvController.php
+++ app/Http/Controllers/MasterDataCctvController.php
@@ -59,7 +59,7 @@ class MasterDataCctvController extends Controller
     public function getAllMasterDataCctv(Request $request)
     {
         try {
-            $dataCctv = MasterDataCctv::all()->sortByDesc('id');
+            $dataCctv = MasterDataCctv::get()->sortByDesc('id');
             return response()->json([
                 'success' => true,
                 'message' => 'Master Data CCTV',
